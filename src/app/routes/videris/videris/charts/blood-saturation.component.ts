cf6ee8fbb7209fa3ea7ddba178548d89109cdf39
Vincent BENOIT <vincent.benoit@qiminfo.ch>
2022-12-24 14:40:17 +0100

    fix: two separate page with routing with different charts

--- src/app/routes/videris/videris/charts/blood-saturation.component.ts
+++ src/app/routes/videris/videris/charts/blood-saturation.component.ts
@@ -1,7 +1,5 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import type { Chart } from '@antv/g2';
-import { ChartEChartsOn, ChartEChartsOption } from '@delon/chart/chart-echarts';
 import { _HttpClient } from '@delon/theme';
 import { NzMessageService } from 'ng-zorro-antd/message';
 
@@ -13,7 +11,6 @@ import { NzMessageService } from 'ng-zorro-antd/message';
     </p>
     <p> blood saturation </p>
     <g2-timeline [data]="offlineChartData" [titleMap]="{ y1: 'abs', y2: 'ord' }"></g2-timeline>
-    <chart-echarts [option]="option"> </chart-echarts>
   `
 })
 export class BloodSaturationChartComponent implements OnInit {
@@ -21,22 +18,6 @@ export class BloodSaturationChartComponent implements OnInit {
   two = false;
 
   offlineChartData!: any[];
-
-  option: ChartEChartsOption = {
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-      type: 'value'
-    },
-    series: [
-      {
-        data: [150, 230, 224, 218, 135, 147, 260],
-        type: 'line'
-      }
-    ]
-  };
 
   constructor(private msg: NzMessageService, private router: Router, private http: _HttpClient, private cdr: ChangeDetectorRef) {}
 
--- src/app/routes/videris/videris/charts/glucose-level.component.ts
+++ src/app/routes/videris/videris/charts/glucose-level.component.ts
@@ -1,42 +1,43 @@
-import { Platform } from '@angular/cdk/platform';
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { OnboardingService } from '@delon/abc/onboarding';
+import { ChartEChartsOption } from '@delon/chart/chart-echarts';
 import { _HttpClient } from '@delon/theme';
 import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'glucose-level-chart',
-  template: ` <p class="">
-    <button (click)="returnToMenu()">Return to menu glucose</button>
-  </p>`
-  //<g2-timeline [data]="offlineChartData" [titleMap]="{ y1: 'abs', y2: 'ord' }"></g2-timeline>
-
-  //<chart-echarts [option]="option"> </chart-echarts>
+  template: `
+    <p class="">
+      <button (click)="returnToMenu()">Return to menu glucose</button>
+    </p>
+    <chart-echarts [option]="option"> </chart-echarts>
+  `
 })
-export class GlucoseLevelChartComponent implements OnInit {
+export class GlucoseLevelChartComponent {
   dark = false;
   two = false;
 
   offlineChartData!: any[];
 
-  constructor(
-    private msg: NzMessageService,
-    private router: Router,
-    private http: _HttpClient,
-    private cdr: ChangeDetectorRef,
-    private obSrv: OnboardingService,
-    private platform: Platform
-  ) {}
+  option: ChartEChartsOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+      }
+    ]
+  };
+
+  constructor(private msg: NzMessageService, private router: Router) {}
 
   returnToMenu() {
     this.router.navigate(['/videris']);
-  }
-
-  ngOnInit(): void {
-    this.http.get('/chart').subscribe(res => {
-      this.offlineChartData = res.offlineChartData;
-      this.cdr.detectChanges();
-    });
   }
 }
